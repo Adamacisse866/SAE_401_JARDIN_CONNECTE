21b1d0372dee870c69ccce49438bc1b5847eccc8
Adamacisse866 <160029010+Adamacisse866@users.noreply.github.com>
2025-03-03 15:48:42 +0100

    Initialisation du projet Flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config.from_object("config.Config")
+
+mysql = MySQL(app)
+
+@app.route("/")
+def index():
+    return "Connexion MySQL réussie !"
+
+@app.route("/events", methods=["GET"])
+def get_events():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM events")
+    events = cur.fetchall()
+    cur.close()
+    
+    return jsonify(events)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = "supersecretkey"
+    MYSQL_HOST = "web-mmi2.iutbeziers.fr"
+    MYSQL_USER = "adama.cisse"
+    MYSQL_PASSWORD = "22313840"
+    MYSQL_DB = "adama.cisse"  
+    MYSQL_CURSORCLASS = "DictCursor"
